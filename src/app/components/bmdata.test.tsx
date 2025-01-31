69aec49d151494936659ef804663a4b86605df50
kubotama <16697573+kubotama@users.noreply.github.com>
2025-01-31 15:41:16 +0900

    fix: replace from category to tags
    ブックマークの分類をカテゴリーからタグに変更した。

--- src/app/components/bmdata.test.tsx
+++ src/app/components/bmdata.test.tsx
@@ -3,18 +3,14 @@ import { BMData } from "./bmdata";
 /***
  * このテストは、BMDataクラスのインスタンスを生成し、そのインスタンスのをテストします。
  * BMDataクラスは、ブックマークのデータを管理するクラスです。
- * BMDataクラスは、カテゴリーを指定することができます。
- * カテゴリーを指定しない場合、ブックマークのデータは空の配列となります。
- * カテゴリーに"github"を指定した場合、ブックマークのデータは、
- * [{"url":"https://github.com/kubotama/linkpage","title":"kubotama/linkpage"}]となります。
- * カテゴリーに"google"を指定した場合、ブックマークのデータは、
- * [{"url":"https://www.google.com/","title":"Google"},{"url":"https://mail.google.com","title":"Gmail"}]となります。
+ * BMDataクラスは、タグを指定することができます。
+ * タグを指定しない場合、ブックマークのデータは空の配列となります。
  */
 
 describe("ブックマークのデータのクラスのテスト", () => {
-  it("カテゴリーを指定しない場合の長さ0の配列が戻る", () => {
-    const category = "";
-    const bmdata = BMData(category);
+  it("タグを指定しない場合の長さ0の配列が戻る", () => {
+    const tag = "";
+    const bmdata = BMData(tag);
     expect(bmdata).toHaveLength(0);
   });
 });
--- src/app/components/bmdata.tsx
+++ src/app/components/bmdata.tsx
@@ -1,5 +1,6 @@
-type Boolmark = { url: string; title: string };
-export const BMData = (category: string): [Boolmark] | [] => {
-  if (category === "") return [];
+type Tags = [] | [string];
+type Boolmark = { url: string; title: string; tags: Tags };
+export const BMData = (tag: string): [Boolmark] | [] => {
+  if (tag === "") return [];
   return [];
 };
