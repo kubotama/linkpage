ff3f4fbacfad016d9303a4932a9a96be9c40b896
kubotama <16697573+kubotama@users.noreply.github.com>
2025-01-31 15:59:35 +0900

    fix: update BMData function to handle 'google' tag correctly
    "google" tag has two bookmarks. So, BMData must handle multi bookmarks.

--- src/app/components/bmdata.test.tsx
+++ src/app/components/bmdata.test.tsx
@@ -7,7 +7,10 @@ import { BMData } from "./bmdata";
  * タグを指定しない場合、ブックマークのデータは空の配列となります。
  * タグに"github"を指定した場合、ブックマークのデータは、
  * [{"url":"https://github.com/kubotama/linkpage","title":"kubotama/linkpage", "tags":["github"]}]となります。
- */
+ * カテゴリーに"google"を指定した場合、ブックマークのデータは、
+ * [{"url":"https://www.google.com/","title":"Google", "tags":["google"]},
+ * {"url":"https://mail.google.com","title":"Gmail","tags":["google"]}]となります。
+ **/
 
 describe("ブックマークのデータのクラスのテスト", () => {
   it("タグを指定しない場合の長さ0の配列が戻る", () => {
@@ -24,6 +27,23 @@ describe("ブックマークのデータのクラスのテスト", () => {
         url: "https://github.com/kubotama/linkpage",
         title: "kubotama/linkpage",
         tags: ["github"],
+      },
+    ]);
+  });
+
+  it("タグに'google'を指定した場合、正しいブックマークデータが戻る", () => {
+    const tag = "google";
+    const bmdata = BMData(tag);
+    expect(bmdata).toEqual([
+      {
+        url: "https://www.google.com/",
+        title: "Google",
+        tags: ["google"],
+      },
+      {
+        url: "https://mail.google.com",
+        title: "Gmail",
+        tags: ["google"],
       },
     ]);
   });
--- src/app/components/bmdata.tsx
+++ src/app/components/bmdata.tsx
@@ -1,6 +1,6 @@
-type Tags = [] | [string];
+type Tags = [] | string[];
 type Boolmark = { url: string; title: string; tags: Tags };
-export const BMData = (tag: string): [Boolmark] | [] => {
+export const BMData = (tag: string): Boolmark[] | [] => {
   if (tag === "") return [];
   if (tag === "github") {
     return [
@@ -8,6 +8,20 @@ export const BMData = (tag: string): [Boolmark] | [] => {
         url: "https://github.com/kubotama/linkpage",
         title: "kubotama/linkpage",
         tags: ["github"],
+      },
+    ];
+  }
+  if (tag === "google") {
+    return [
+      {
+        url: "https://www.google.com/",
+        title: "Google",
+        tags: ["google"],
+      },
+      {
+        url: "https://mail.google.com",
+        title: "Gmail",
+        tags: ["google"],
       },
     ];
   }
