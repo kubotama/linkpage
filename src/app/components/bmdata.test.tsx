8907e2d572bd29f5fbbaab71246c50330fd546a8
kubotama <16697573+kubotama@users.noreply.github.com>
2025-01-31 15:49:50 +0900

    fix: update BMData function to return correct bookmark data based on tag

--- src/app/components/bmdata.test.tsx
+++ src/app/components/bmdata.test.tsx
@@ -5,6 +5,8 @@ import { BMData } from "./bmdata";
  * BMDataクラスは、ブックマークのデータを管理するクラスです。
  * BMDataクラスは、タグを指定することができます。
  * タグを指定しない場合、ブックマークのデータは空の配列となります。
+ * タグに"github"を指定した場合、ブックマークのデータは、
+ * [{"url":"https://github.com/kubotama/linkpage","title":"kubotama/linkpage", "tags":["github"]}]となります。
  */
 
 describe("ブックマークのデータのクラスのテスト", () => {
@@ -12,5 +14,17 @@ describe("ブックマークのデータのクラスのテスト", () => {
     const tag = "";
     const bmdata = BMData(tag);
     expect(bmdata).toHaveLength(0);
+  });
+
+  it("タグに'github'を指定した場合、正しいブックマークデータが戻る", () => {
+    const tag = "github";
+    const bmdata = BMData(tag);
+    expect(bmdata).toEqual([
+      {
+        url: "https://github.com/kubotama/linkpage",
+        title: "kubotama/linkpage",
+        tags: ["github"],
+      },
+    ]);
   });
 });
--- src/app/components/bmdata.tsx
+++ src/app/components/bmdata.tsx
@@ -2,5 +2,14 @@ type Tags = [] | [string];
 type Boolmark = { url: string; title: string; tags: Tags };
 export const BMData = (tag: string): [Boolmark] | [] => {
   if (tag === "") return [];
+  if (tag === "github") {
+    return [
+      {
+        url: "https://github.com/kubotama/linkpage",
+        title: "kubotama/linkpage",
+        tags: ["github"],
+      },
+    ];
+  }
   return [];
 };
