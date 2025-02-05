3ed2148ae5e63191ae1db6cd2f131907c3b12887
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-05 23:54:57 +0900

    update: Creating a grid of bookmarks has become a component
    ブックマークが変更されたときに、反映されるように修正した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import { Bookmarks } from "./components/bookmark";
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+import { Bookmarks, Bookmark } from "./components/bookmark";
 
 export default function Home() {
-  const bookmarks = Bookmarks();
   return (
     <div>
       <div>linkpage</div>
-      <div className="grid grid-cols-1">
+      <BookmarkGrid />
+    </div>
+  );
+}
+
+const BookmarkGrid = () => {
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+  const [bookmarkGrid, setBookmarkGrid] = useState(<div></div>);
+
+  if (bookmarks.length === 0) {
+    setBookmarks(Bookmarks());
+  }
+
+  useEffect(() => {
+    setBookmarkGrid(() => (
+      <>
         {bookmarks.map((bookmark, index) => (
           <div className="grid-item" key={index}>
             <a href={bookmark.url} target="_blank">
@@ -13,7 +31,8 @@ export default function Home() {
             </a>
           </div>
         ))}
-      </div>
-    </div>
-  );
-}
+      </>
+    ));
+  }, [bookmarks]);
+  return <div className="grid grid-cols-1">{bookmarkGrid}</div>;
+};
