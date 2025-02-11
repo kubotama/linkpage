5ef054747ff24f327b1d83240351248eae13baa6
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-11 23:13:04 +0900

    Simplified bookmark display algorithm

--- src/app/components/bmGrid.tsx
+++ src/app/components/bmGrid.tsx
@@ -11,13 +11,13 @@ export const BmGrid: React.FC<{ bookmarks: Bookmark[] }> = ({ bookmarks }) => {
   const [bookmarkGrid, setBookmarkGrid] = useState(<div></div>);
 
   useEffect(() => {
-    setBookmarkGrid(() => (
+    setBookmarkGrid(
       <>
         {bookmarks.map((bookmark, index) => (
           <BmRow key={index} bookmark={bookmark} />
         ))}
       </>
-    ));
+    );
   }, [bookmarks]);
 
   return <div className="grid grid-cols-1">{bookmarkGrid}</div>;
