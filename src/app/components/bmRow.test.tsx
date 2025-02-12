4010c1cfab6b999d09d706a79b28e1bbdaba3c0e
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-12 22:30:34 +0900

    recover the bmRow component tests

---
+++ src/app/components/bmRow.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import { BmRow } from "./bmRow";
+
+/***
+ * このテストは、ブックマークのリンクのインスタンスを生成して、そのインスタンスが正しいかをテストする。
+ */
+
+describe("ブックマークのリンクのテスト", () => {
+  it("ブックマークのリンク(GitHub)のインスタンスが生成できる", () => {
+    const bookmark = {
+      url: "https://github.com/kubotama/linkpage",
+      title: "kubotama/linkpage",
+    };
+    render(<BmRow bookmark={bookmark} key={1} />);
+    const link = screen.getByText("kubotama/linkpage");
+    expect(link).toHaveAttribute("href", bookmark.url);
+  });
+
+  it("ブックマークのリンク(Gmail)のインスタンスが生成できる", () => {
+    const bookmark = {
+      url: "https://mail.google.com",
+      title: "Gmail",
+    };
+    render(<BmRow bookmark={bookmark} key={2} />);
+    const link = screen.getByText("Gmail");
+    expect(link).toHaveAttribute("href", bookmark.url);
+  });
+});
