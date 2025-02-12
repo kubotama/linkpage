0bc1222e9aff18e63317b9ab0d65d547a1cdd53e
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-12 21:49:03 +0900

    Successfully replaced API calls with mocks

--- src/app/bookmark/route.test.ts
+++ src/app/bookmark/route.test.ts
@@ -1,10 +1,36 @@
 import fetch from "cross-fetch";
 
 import "@testing-library/jest-dom";
+import fetchMock from "jest-fetch-mock";
 
 describe("ブックマークのAPIのテスト", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
   it("ブックマークのデータが取得できる", () => {
-    const url = "http://localhost:3001/bookmark";
+    const url = "http://localhost:3000/bookmark";
+    const mockBookmarks = [
+      {
+        url: "https://github.com/kubotama/linkpage",
+        title: "kubotama/linkpage",
+      },
+      {
+        url: "https://www.google.com/",
+        title: "Google",
+      },
+      {
+        url: "https://mail.google.com",
+        title: "Gmail",
+      },
+      {
+        url: "https://www.amazon.co.jp/",
+        title: "Amazon",
+      },
+    ];
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
     return fetch(url).then((response) => {
       response.json().then((json) => {
         expect(json).toEqual([
