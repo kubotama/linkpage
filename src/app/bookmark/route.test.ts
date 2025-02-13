0be9b392540edaeb68f8f8ecb6d4a1ac37a25438
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-13 20:50:22 +0900

    Added tests for API that reads and returns bookmarks from a file

--- src/app/bookmark/route.test.ts
+++ src/app/bookmark/route.test.ts
@@ -1,16 +1,17 @@
-import fetch from "cross-fetch";
+// import fetch from "cross-fetch";
 
 import "@testing-library/jest-dom";
-import fetchMock from "jest-fetch-mock";
+// import fetchMock from "jest-fetch-mock";
 import { Bookmark } from "../page";
+import { GET } from "./route";
 
 describe("ブックマークのAPIのテスト", () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
+  // beforeEach(() => {
+  //   fetchMock.resetMocks();
+  // });
 
-  it("ブックマークのデータが取得できる", () => {
-    const url = "http://localhost:3030/bookmark";
+  it("ブックマークのデータが取得できる", async () => {
+    // const url = "http://localhost:3030/bookmark";
     const mockBookmarks: Bookmark[] = [
       {
         url: "https://github.com/kubotama/linkpage",
@@ -30,29 +31,34 @@ describe("ブックマークのAPIのテスト", () => {
       },
     ];
 
-    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+    // fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
 
-    return fetch(url).then((response) => {
-      response.json().then((json) => {
-        expect(json).toEqual([
-          {
-            url: "https://github.com/kubotama/linkpage",
-            title: "kubotama/linkpage",
-          },
-          {
-            url: "https://www.google.com/",
-            title: "Google",
-          },
-          {
-            url: "https://mail.google.com",
-            title: "Gmail",
-          },
-          {
-            url: "https://www.amazon.co.jp/",
-            title: "Amazon",
-          },
-        ]);
-      });
-    });
+    const response = await GET();
+    expect(response.status).toBe(200);
+    const json = await response.json();
+    expect(json).toEqual(mockBookmarks);
+
+    // return fetch(url).then((response) => {
+    //   response.json().then((json) => {
+    //     expect(json).toEqual([
+    //       {
+    //         url: "https://github.com/kubotama/linkpage",
+    //         title: "kubotama/linkpage",
+    //       },
+    //       {
+    //         url: "https://www.google.com/",
+    //         title: "Google",
+    //       },
+    //       {
+    //         url: "https://mail.google.com",
+    //         title: "Gmail",
+    //       },
+    //       {
+    //         url: "https://www.amazon.co.jp/",
+    //         title: "Amazon",
+    //       },
+    //     ]);
+    //   });
+    // });
   });
 });
