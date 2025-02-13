5b7b4e2ca31a927011bb1f1239307196413e492a
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-13 21:28:24 +0900

    deleted the programs that were commented out
    モックを利用しなくなったので、
    変数名(mockBookmarks)を、ふさわしい名称に変更した

--- src/app/bookmark/route.test.ts
+++ src/app/bookmark/route.test.ts
@@ -1,18 +1,13 @@
 // import fetch from "cross-fetch";
 
-import "@testing-library/jest-dom";
-// import fetchMock from "jest-fetch-mock";
-import { Bookmark } from "../page";
-import { GET } from "./route";
+import '@testing-library/jest-dom';
+
+import { Bookmark } from '../page';
+import { GET } from './route';
 
 describe("ブックマークのAPIのテスト", () => {
-  // beforeEach(() => {
-  //   fetchMock.resetMocks();
-  // });
-
   it("ブックマークのデータが取得できる", async () => {
-    // const url = "http://localhost:3030/bookmark";
-    const mockBookmarks: Bookmark[] = [
+    const bookmarks: Bookmark[] = [
       {
         url: "https://github.com/kubotama/linkpage",
         title: "kubotama/linkpage",
@@ -31,34 +26,9 @@ describe("ブックマークのAPIのテスト", () => {
       },
     ];
 
-    // fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
-
     const response = await GET();
     expect(response.status).toBe(200);
     const json = await response.json();
-    expect(json).toEqual(mockBookmarks);
-
-    // return fetch(url).then((response) => {
-    //   response.json().then((json) => {
-    //     expect(json).toEqual([
-    //       {
-    //         url: "https://github.com/kubotama/linkpage",
-    //         title: "kubotama/linkpage",
-    //       },
-    //       {
-    //         url: "https://www.google.com/",
-    //         title: "Google",
-    //       },
-    //       {
-    //         url: "https://mail.google.com",
-    //         title: "Gmail",
-    //       },
-    //       {
-    //         url: "https://www.amazon.co.jp/",
-    //         title: "Amazon",
-    //       },
-    //     ]);
-    //   });
-    // });
+    expect(json).toEqual(bookmarks);
   });
 });
