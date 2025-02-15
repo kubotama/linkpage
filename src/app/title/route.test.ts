91f5793124a65d15bdd7a649bee19f59d8ff7a46
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-15 16:39:32 +0900

    simplified the tests
    テストを非同期化(asyncを関数の定義に追加)することで、読みやすくした。

--- src/app/title/route.test.ts
+++ src/app/title/route.test.ts
@@ -8,35 +8,31 @@ describe("タイトルを取得するAPIのテスト", () => {
     fetchMock.resetMocks();
   });
 
-  it("github.com/kubotama/linkpage", () => {
+  it("github.com/kubotama/linkpage", async () => {
     const url = "https://github.com/kubotama/linkpage";
     fetchMock.mockResponseOnce(
       "<html><head><title>link page</title></head><body></body></html>"
     );
 
-    return GET(url).then((response) => {
-      expect(response.status).toBe(200);
-      expect(fetchMock.mock.calls.length).toEqual(1);
-      expect(fetchMock.mock.calls[0][0]).toEqual(url); // Update the expected URL here
-      return response.text().then((text) => {
-        expect(text).toBe("link page");
-      });
-    });
+    const response = await GET(url);
+    expect(response.status).toBe(200);
+    expect(fetchMock.mock.calls.length).toEqual(1);
+    expect(fetchMock.mock.calls[0][0]).toEqual(url); // Update the expected URL here
+    const text = await response.text();
+    expect(text).toBe("link page");
   });
 
-  it("https://www.google.com/", () => {
+  it("https://www.google.com/", async () => {
     const url = "https://www.google.com/";
     fetchMock.mockResponseOnce(
       "<html><head><title>Google</title></head><body></body></html>"
     );
 
-    return GET(url).then((response) => {
-      expect(response.status).toBe(200);
-      expect(fetchMock.mock.calls.length).toEqual(1);
-      expect(fetchMock.mock.calls[0][0]).toEqual(url); // Update the expected URL here
-      return response.text().then((text) => {
-        expect(text).toBe("Google");
-      });
-    });
+    const response = await GET(url);
+    expect(response.status).toBe(200);
+    expect(fetchMock.mock.calls.length).toEqual(1);
+    expect(fetchMock.mock.calls[0][0]).toEqual(url); // Update the expected URL here
+    const text = await response.text();
+    expect(text).toBe("Google");
   });
 });
