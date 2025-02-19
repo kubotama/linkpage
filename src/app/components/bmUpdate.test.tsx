88e79295a46384ab3c8a5474ce0be4fc7b5f0e06
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-19 20:37:19 +0900

    Added a function to call an API to get the title when the button to get the title is clicked.

--- src/app/components/bmUpdate.test.tsx
+++ src/app/components/bmUpdate.test.tsx
@@ -1,12 +1,15 @@
-// MyComponent.test.tsx
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import fetchMock from "jest-fetch-mock";
 
-// import MyComponent from './MyComponent';
 import { BmUpdate } from "./bmUpdate";
 
 describe("BmUpdate", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
   it("入力された文字列が親コンポーネントに渡されること", () => {
     const url = "https://mail.google.com/mail/";
     const onBmUpdate = jest.fn(); // モック関数を作成
@@ -36,9 +39,30 @@ describe("BmUpdate", () => {
     expect(screen.getByText("更新")).toBeInTheDocument();
   });
 
-  it("タイトルを取得するボタンをクリック", () => {
+  it("タイトルを取得するボタンをクリック", async () => {
     // タイトルを取得するボタンをクリックすると、タイトルを取得するAPIを呼び出す。
     // パラメータとしてURLのテキストボックスに入力された文字列が渡される。
     // タイトルのテキストボックスに、APIから返されたタイトルが表示される。
+    const url = "https://mail.google.com/mail/";
+    const title = "Gmail";
+    const onBmUpdate = jest.fn(); // モック関数を作成
+
+    render(<BmUpdate onBmUpdate={onBmUpdate} />);
+    const urlInput = screen.getByLabelText("url");
+    const titleInput = screen.getByLabelText("title") as HTMLInputElement;
+    const titleButton = screen.getByText("タイトル");
+
+    fetchMock.mockResponseOnce(title);
+
+    fireEvent.change(urlInput, {
+      target: { value: url },
+    });
+    fireEvent.click(titleButton);
+
+    await waitFor(() => {
+      expect(titleInput.value).toEqual(title);
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual("/title?url=" + url);
+    });
   });
 });
--- src/app/components/bmUpdate.tsx
+++ src/app/components/bmUpdate.tsx
@@ -13,12 +13,22 @@ export const BmUpdate: React.FC<BmUpdateProps> = ({
   const textUrlRef1 = useRef<HTMLInputElement>(null);
   const textTitleRef2 = useRef<HTMLInputElement>(null);
 
-  const handleClick = () => {
+  const updateClick = () => {
     if (textUrlRef1.current && textTitleRef2.current) {
       const inputTextUrl = textUrlRef1.current.value;
       const inputTextTitle = textTitleRef2.current.value;
       onBmUpdate(inputTextUrl, inputTextTitle);
     }
+  };
+
+  const titleClick = () => {
+    fetch("/title?url=" + textUrl)
+      .then((response) => {
+        return response.text();
+      })
+      .then((text) => {
+        setTextTitle(text);
+      });
   };
 
   return (
@@ -37,8 +47,8 @@ export const BmUpdate: React.FC<BmUpdateProps> = ({
         onChange={(e) => setTextTitle(e.target.value)}
         ref={textTitleRef2}
       />
-      <button>タイトル</button>
-      <button onClick={handleClick}>更新</button>
+      <button onClick={titleClick}>タイトル</button>
+      <button onClick={updateClick}>更新</button>
     </div>
   );
 };
