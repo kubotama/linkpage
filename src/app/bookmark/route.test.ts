dad356e61161483263839a91766558ddf47f4d41
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-20 23:28:21 +0900

    Replaced the loading of bookmark files with a mock.

--- src/app/bookmark/route.test.ts
+++ src/app/bookmark/route.test.ts
@@ -1,30 +1,35 @@
 // import fetch from "cross-fetch";
 
 import "@testing-library/jest-dom";
+import * as fs from "fs";
 
 import { Bookmark } from "../components/bmRow";
 import { GET } from "./route";
 
+jest.mock("fs");
+
+const bookmarks: Bookmark[] = [
+  {
+    url: "https://github.com/kubotama/linkpage",
+    title: "kubotama/linkpage",
+  },
+  {
+    url: "https://www.google.com/",
+    title: "Google",
+  },
+  {
+    url: "https://mail.google.com",
+    title: "Gmail",
+  },
+  {
+    url: "https://www.amazon.co.jp/",
+    title: "Amazon",
+  },
+];
+
 describe("ブックマークのAPIのテスト", () => {
   it("ブックマークのデータが取得できる", async () => {
-    const bookmarks: Bookmark[] = [
-      {
-        url: "https://github.com/kubotama/linkpage",
-        title: "kubotama/linkpage",
-      },
-      {
-        url: "https://www.google.com/",
-        title: "Google",
-      },
-      {
-        url: "https://mail.google.com",
-        title: "Gmail",
-      },
-      {
-        url: "https://www.amazon.co.jp/",
-        title: "Amazon",
-      },
-    ];
+    (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(bookmarks));
 
     const response = await GET();
     expect(response.status).toBe(200);
