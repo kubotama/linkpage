628ab9de52b96576c185a1325a0fe9245faa6d07
kubotama <16697573+kubotama@users.noreply.github.com>
2025-02-25 21:16:15 +0900

    test: Homeコンポーネントのタイトル表示テストを追加
    このコミットでは、タイトルの表示をBmMessageに変更しました。
    あわせて、Homeコンポーネントの以下のシナリオをカバーするテストを追加しました：
    - 初期表示時に「linkpage」が表示されることを確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { BmGrid } from "./components/bmGrid";
+import BmMessage from "./components/bmMessage";
+import { MessageProvider } from "./contexts/MessageContext";
 
 export const Home: React.FC = () => {
   return (
-    <>
-      <div>linkpage</div>
+    <MessageProvider>
+      <BmMessage />
       <BmGrid />
-    </>
+    </MessageProvider>
   );
 };
 
