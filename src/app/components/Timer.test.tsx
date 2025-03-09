f66f4e39355f076efa5f24cb997123c171731d32
kubotama <16697573+kubotama@users.noreply.github.com>
2025-03-09 21:04:32 +0900

    開始ボタンをクリックすると停止ボタンが表示される
    開始ボタンは表示されていない。

--- src/app/components/Timer.test.tsx
+++ src/app/components/Timer.test.tsx
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 import React from "react";
 
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Timer } from "./Timer";
 
@@ -13,6 +14,17 @@ describe("Timer", () => {
       expect(screen.getByText("02:30")).toBeInTheDocument();
       expect(screen.getByRole("button", { name: "開始" })).toBeInTheDocument();
       expect(screen.queryByRole("button", { name: "停止" })).toBeNull();
+    });
+  });
+
+  it("開始ボタンを押すと停止ボタンが表示される", async () => {
+    const user = userEvent.setup();
+
+    render(<Timer />);
+    await user.click(screen.getByRole("button", { name: "開始" }));
+    await waitFor(() => {
+      expect(screen.getByRole("button", { name: "停止" })).toBeInTheDocument();
+      expect(screen.queryByRole("button", { name: "開始" })).toBeNull();
     });
   });
 });
--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -3,8 +3,18 @@ import React, { useState } from "react";
 import { Button } from "@mui/material";
 
 export const Timer: React.FC = () => {
-  const startTimer = <Button>開始</Button>;
+  const handlerStartTimer = () => {
+    setButtonTimer(stopTimer);
+  };
+
+  const handlerStopTimer = () => {
+    setButtonTimer(startTimer);
+  };
+
+  const startTimer = <Button onClick={handlerStartTimer}>開始</Button>;
+  const stopTimer = <Button onClick={handlerStopTimer}>停止</Button>;
   const [buttonTimer, setButtonTimer] = useState(startTimer);
+
   return (
     <div>
       {buttonTimer}
