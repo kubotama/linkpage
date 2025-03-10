ebe584a174007552770f5f78cdfbb8872b81a85b
kubotama <16697573+kubotama@users.noreply.github.com>
2025-03-10 22:15:37 +0900

    タイマーを追加する Fixes #42
    設定されている時間で、自動的に繰り返しアラームが鳴る。
    停止ボタンをクリックした後に、開始ボタンをクリックすると設定されている時間にリセットされてタイマーを開始する。

--- src/app/components/Timer.test.tsx
+++ src/app/components/Timer.test.tsx
@@ -1,62 +1,111 @@
 import "@testing-library/jest-dom";
-
 import React from "react";
-import { Button } from "@mui/material";
-
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-
 import { Timer } from "./Timer";
+import { useTimer } from "react-timer-hook";
 
-describe("Timer", () => {
-  it("タイマーが表示される", async () => {
-    render(<Timer />);
-    await waitFor(() => {
-      expect(screen.getByText("02:30")).toBeInTheDocument();
-      expect(screen.getByRole("button", { name: "開始" })).toBeInTheDocument();
-      expect(screen.queryByRole("button", { name: "停止" })).toBeNull();
+// モック関数を作成
+const mockUseTimer = useTimer as jest.Mock;
+const mockPlayBeep = jest.fn();
+const mockStart = jest.fn();
+const mockPause = jest.fn();
+
+jest.mock("react-timer-hook", () => ({
+  useTimer: jest.fn(),
+}));
+
+describe("Timer コンポーネント", () => {
+  beforeEach(() => {
+    mockStart.mockClear();
+    mockPause.mockClear();
+    mockUseTimer.mockReturnValue({
+      seconds: 0,
+      minutes: 0,
+      isRunning: false,
+      start: mockStart,
+      pause: mockPause,
+      onExpire: mockPlayBeep,
     });
+    mockPlayBeep.mockClear();
   });
 
-  it("開始ボタンを押すと停止ボタンが表示される", async () => {
-    const user = userEvent.setup();
-
-    render(<Timer />);
-    await user.click(screen.getByRole("button", { name: "開始" }));
-    await waitFor(() => {
-      expect(screen.getByRole("button", { name: "停止" })).toBeInTheDocument();
-      expect(screen.queryByRole("button", { name: "開始" })).toBeNull();
+  it("初期画面で 01:15 と開始ボタンが表示されること", () => {
+    mockUseTimer.mockReturnValue({
+      seconds: 15,
+      minutes: 1,
+      isRunning: false,
+      start: mockStart,
+      pause: mockPause,
+      onExpire: mockPlayBeep,
     });
+    render(<Timer />);
+    expect(screen.getByText("01:15")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "開始" })).toBeInTheDocument();
   });
 
-  it("停止ボタンを押すと開始ボタンが表示される", async () => {
+  it("開始ボタンをクリックするとhandleClickが呼び出されること", async () => {
     const user = userEvent.setup();
-
-    render(<Timer />);
-    await user.click(screen.getByRole("button", { name: "開始" }));
-    await user.click(screen.getByRole("button", { name: "停止" }));
-    await waitFor(() => {
-      expect(screen.getByRole("button", { name: "開始" })).toBeInTheDocument();
-      expect(screen.queryByRole("button", { name: "停止" })).toBeNull();
-    });
+    const { getByRole } = render(<Timer />);
+    const startButton = getByRole("button", { name: "開始" });
+    await user.click(startButton);
+    expect(mockStart).toHaveBeenCalled();
   });
 
-  it("開始ボタンがクリックされたら、handlerStartTimerが呼び出されたことを確認する", async () => {
+  it("開始ボタンをクリックすると停止ボタンが表示されること", async () => {
     const user = userEvent.setup();
-    const handlerStartTimerMock = jest.fn();
-    //mock implementation to avoid changing internal state of component
-    jest
-      .spyOn(React, "useState")
-      .mockReturnValueOnce([
-        <Button key="start" onClick={handlerStartTimerMock}>
-          開始
-        </Button>,
-        () => {},
-      ])
-      .mockReturnValueOnce([<Button key="stop">停止</Button>, () => {}]);
+
+    const { getByRole } = render(<Timer />);
+    const startButton = getByRole("button", { name: "開始" });
+    await user.click(startButton);
+    expect(getByRole("button", { name: "停止" })).toBeInTheDocument();
+  });
+
+  it("停止ボタンをクリックすると開始ボタンが表示されること", () => {
+    const { getByRole } = render(<Timer />);
+    const startButton = getByRole("button", { name: "開始" });
+
+    act(() => {
+      userEvent.click(startButton);
+      mockUseTimer.mockReturnValue({
+        seconds: 30,
+        minutes: 2,
+        isRunning: true,
+        start: mockStart,
+        pause: mockPause,
+        onExpire: mockPlayBeep,
+      });
+    });
+
+    const stopButton = getByRole("button", { name: "停止" });
+
+    act(() => {
+      userEvent.click(stopButton);
+      mockUseTimer.mockReturnValue({
+        seconds: 30,
+        minutes: 2,
+        isRunning: false,
+        start: mockStart,
+        pause: mockPause,
+        onExpire: mockPlayBeep,
+      });
+    });
+
+    expect(getByRole("button", { name: "開始" })).toBeInTheDocument();
+  });
+
+  it("onExpire が呼び出されること", () => {
     render(<Timer />);
-    await user.click(screen.getByRole("button", { name: "開始" }));
-    expect(handlerStartTimerMock).toHaveBeenCalledTimes(1);
-    jest.restoreAllMocks();
+    act(() => {
+      mockUseTimer.mockReturnValue({
+        seconds: 0,
+        minutes: 0,
+        isRunning: false,
+        start: mockStart,
+        pause: mockPause,
+        onExpire: mockPlayBeep,
+      });
+    });
+    expect(mockPlayBeep).toHaveBeenCalled();
   });
 });
--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -1,24 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useTimer } from "react-timer-hook";
 
 import { Button } from "@mui/material";
 
+const DURATION_TIME = 160;
 export const Timer: React.FC = () => {
-  const handlerStartTimer = () => {
-    setButtonTimer(stopTimer);
+  const [buttonTimer, setButtonTimer] = useState("開始");
+  const [isStarted, setIsStarted] = useState(false);
+  // const [durationTime] = useState(DURATION_TIME);
+  // const [mm, setMm] = useState(0);
+  // const [ss, setSs] = useState(0);
+
+  //  簡易的なビープ音再生
+  function playBeep() {
+    // 型定義を拡張
+    const AudioContext =
+      // window.AudioContext || (window as any).webkitAudioContext;
+      window.AudioContext;
+
+    const audioCtx = new AudioContext();
+    const oscillator = audioCtx.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+    oscillator.connect(audioCtx.destination);
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + 0.5);
+    // oscillator.disconnect();
+  }
+
+  // expiryTimestamp を生成するヘルパー
+  const getExpiryTimestamp = (duration: number): Date => {
+    return new Date(Date.now() + duration * 1000);
   };
 
-  const handlerStopTimer = () => {
-    setButtonTimer(startTimer);
+  // const handleExpire = () => {
+  //   // setMm(Math.floor(DURATION_TIME / 60));
+  //   // setSs(DURATION_TIME % 60);
+  //   playBeep();
+  //   // start();
+  //   // restart(getExpiryTimestamp(DURATION_TIME));
+  // };
+
+  const { seconds, minutes, isRunning, restart, pause } = useTimer({
+    expiryTimestamp: getExpiryTimestamp(DURATION_TIME),
+    autoStart: false,
+    // onExpire: () => handleExpire(),
+  });
+
+  const handlerClick = () => {
+    if (!isRunning) {
+      setIsStarted(true);
+      restart(getExpiryTimestamp(DURATION_TIME));
+      // start();
+    } else {
+      pause();
+      setIsStarted(false);
+    }
   };
 
-  const startTimer = <Button onClick={handlerStartTimer}>開始</Button>;
-  const stopTimer = <Button onClick={handlerStopTimer}>停止</Button>;
-  const [buttonTimer, setButtonTimer] = useState(startTimer);
+  useEffect(() => {
+    if (isRunning) {
+      setButtonTimer("停止");
+    } else {
+      setButtonTimer("開始");
+      if (isStarted) {
+        playBeep();
+        restart(getExpiryTimestamp(DURATION_TIME));
+        // } else {
+        //   setIsStarted(true);
+      }
+      // start();
+    }
+  }, [isRunning, isStarted, restart]);
+
+  // useEffect(() => {
+  //   setMm(minutes);
+  //   setSs(seconds);
+  // }, [minutes, seconds]);
 
   return (
     <div>
-      {buttonTimer}
-      {"02:30"}
+      <Button onClick={handlerClick} variant="contained">
+        {buttonTimer}
+      </Button>
+      {minutes.toString().padStart(2, "0")}:
+      {seconds.toString().padStart(2, "0")}
     </div>
   );
 };
