1db6d0aee5781d7768719494912ca0ba6fe5660d
kubotama <16697573+kubotama@users.noreply.github.com>
2025-03-11 22:27:38 +0900

    test(Timer): 実行中は、タイマーの残り時間と停止ボタンが表示されていることを確認するテストを追加した

--- src/app/components/Timer.test.tsx
+++ src/app/components/Timer.test.tsx
@@ -1,23 +1,34 @@
 import "@testing-library/jest-dom";
 import React from "react";
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, act, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Timer } from "./Timer";
 
 describe("Timer コンポーネント", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it("初期画面で 01:15 と開始ボタンが表示されること", () => {
     render(<Timer durationTime={10} />);
     expect(screen.getByText("00:10")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "開始" })).toBeInTheDocument();
   });
 
-  it.skip("開始ボタンをクリックすると停止ボタンが表示されること", async () => {
+  it("開始ボタンをクリックすると停止ボタンが表示されること", async () => {
     const user = userEvent.setup();
 
-    const { getByRole } = render(<Timer durationTime={170} />);
+    const { getByRole, getByText } = render(<Timer durationTime={150} />);
     const startButton = getByRole("button", { name: "開始" });
-    await user.click(startButton);
-    expect(getByRole("button", { name: "停止" })).toBeInTheDocument();
+    user.click(startButton).then(() => {
+      jest.advanceTimersByTime(30000); // 30秒進める
+
+      expect(getByRole("button", { name: "停止" })).toBeInTheDocument();
+      expect(getByText("02:00")).toBeInTheDocument();
+    });
   });
 
   it.skip("停止ボタンをクリックすると開始ボタンが表示されること", () => {
