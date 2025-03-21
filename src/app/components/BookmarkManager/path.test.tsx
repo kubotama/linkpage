16ba0ea42dd12294ee873d2631219874aa57e597
kubotama <16697573+kubotama@users.noreply.github.com>
2025-03-21 23:04:27 +0900

    test: ユニットテストファイルを分割して整理
    「←」ボタンのテストを、別ファイルに分割した。

---
+++ src/app/components/BookmarkManager/path.test.tsx
@@ -0,0 +1,165 @@
+import "@testing-library/jest-dom";
+
+import fetchMock from "jest-fetch-mock";
+import React, { act } from "react";
+
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+
+import { MessageProvider } from "../../contexts/MessageContext";
+import BmMessage from "../bmMessage";
+import { Bookmark, BookmarkManager } from "../BookmarkManager";
+
+const mockBookmarks: Bookmark[] = [
+  {
+    url: "https://github.com/kubotama/linkpage",
+    title: "kubotama/linkpage",
+  },
+  {
+    url: "https://www.google.com/",
+    title: "Google",
+  },
+  {
+    url: "https://mail.google.com",
+    title: "Gmail",
+  },
+  {
+    url: "https://www.amazon.co.jp/",
+    title: "Amazon",
+  },
+];
+
+describe("「←」ボタン: URLから、/の階層を一段、削除する", () => {
+  it("https://mail.google.com/mail/u/0/", async () => {
+    const url = "https://mail.google.com/mail/u/0/#inbox";
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
+    await act(async () => {
+      render(
+        <MessageProvider>
+          <BmMessage />
+          <BookmarkManager />
+        </MessageProvider>
+      );
+    });
+
+    const urlInput = screen.getByRole("textbox", { name: "url" });
+    const pathButton = screen.getByRole("button", { name: "←" });
+
+    await act(async () => {
+      fireEvent.change(urlInput, { target: { value: url } });
+      fireEvent.click(pathButton);
+    });
+
+    await waitFor(() => {
+      expect(urlInput).toHaveValue("https://mail.google.com/mail/u/0/");
+    });
+  });
+
+  it("https://xtech.nikkei.com/atcl/nxt/column/18/00148/030500376", async () => {
+    const url = "https://xtech.nikkei.com/atcl/nxt/column/18/00148/030500376";
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
+    await act(async () => {
+      render(
+        <MessageProvider>
+          <BmMessage />
+          <BookmarkManager />
+        </MessageProvider>
+      );
+    });
+
+    const urlInput = screen.getByRole("textbox", { name: "url" });
+    const pathButton = screen.getByRole("button", { name: "←" });
+
+    await act(async () => {
+      fireEvent.change(urlInput, { target: { value: url } });
+      fireEvent.click(pathButton);
+    });
+
+    await waitFor(() => {
+      expect(urlInput).toHaveValue(
+        "https://xtech.nikkei.com/atcl/nxt/column/18/00148/"
+      );
+    });
+  });
+
+  it("https://xtech.nikkei.com/atcl/nxt/column/18/00148/030500376/", async () => {
+    const url = "https://xtech.nikkei.com/atcl/nxt/column/18/00148/030500376/";
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
+    await act(async () => {
+      render(
+        <MessageProvider>
+          <BmMessage />
+          <BookmarkManager />
+        </MessageProvider>
+      );
+    });
+
+    const urlInput = screen.getByRole("textbox", { name: "url" });
+    const pathButton = screen.getByRole("button", { name: "←" });
+
+    await act(async () => {
+      fireEvent.change(urlInput, { target: { value: url } });
+      fireEvent.click(pathButton);
+    });
+
+    await waitFor(() => {
+      expect(urlInput).toHaveValue(
+        "https://xtech.nikkei.com/atcl/nxt/column/18/00148/"
+      );
+    });
+  });
+
+  it("https://xtech.nikkei.com", async () => {
+    const url = "https://xtech.nikkei.com";
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
+    await act(async () => {
+      render(
+        <MessageProvider>
+          <BmMessage />
+          <BookmarkManager />
+        </MessageProvider>
+      );
+    });
+    const urlInput = screen.getByRole("textbox", { name: "url" });
+    const pathButton = screen.getByRole("button", { name: "←" });
+
+    await act(async () => {
+      fireEvent.change(urlInput, { target: { value: url } });
+      fireEvent.click(pathButton);
+    });
+
+    await waitFor(() => {
+      expect(urlInput).toHaveValue("https://xtech.nikkei.com");
+    });
+  });
+
+  it("https://xtech.nikkei.com/", async () => {
+    const url = "https://xtech.nikkei.com/";
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockBookmarks));
+
+    await act(async () => {
+      render(
+        <MessageProvider>
+          <BmMessage />
+          <BookmarkManager />
+        </MessageProvider>
+      );
+    });
+
+    const urlInput = screen.getByRole("textbox", { name: "url" });
+    const pathButton = screen.getByRole("button", { name: "←" });
+
+    await act(async () => {
+      fireEvent.change(urlInput, { target: { value: url } });
+      fireEvent.click(pathButton);
+    });
+
+    await waitFor(() => {
+      expect(urlInput).toHaveValue("https://xtech.nikkei.com/");
+    });
+  });
+});
