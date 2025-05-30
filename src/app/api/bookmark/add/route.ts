5c30bf2de2cd5ed26c97fea3207c8adff0751788
kubotama <16697573+kubotama@users.noreply.github.com>
2025-05-30 13:29:07 +0900

    refactor: ブックマークの重複チェック処理を簡素化し、エラーレスポンスを修正

--- src/app/api/bookmark/add/route.ts
+++ src/app/api/bookmark/add/route.ts
@@ -14,45 +14,40 @@ export const POST: (request: Request) => Promise<Response> = async (
       return new Response("Title cannot be empty", { status: 400 });
     }
 
-    try {
-      const db = getDb();
-      const existingBookmark = db
-        .prepare("SELECT * FROM bookmarks WHERE url = ?")
-        .get(bookmark.url);
+    const db = getDb();
+    const existingBookmark = db
+      .prepare("SELECT * FROM bookmarks WHERE url = ?")
+      .get(bookmark.url);
 
-      if (existingBookmark) {
-        return new Response(
-          JSON.stringify({
-            error: "指定されたURLのブックマークは既に登録されています。",
-            message: "指定されたURLのブックマークは既に登録されています。",
-            url: bookmark.url,
-            title: bookmark.title,
-          }),
-          {
-            status: 409,
-            headers: { "Content-Type": "application/json" },
-          }
-        );
-      }
-      // try {
-      const insertStmt = db.prepare(
-        "INSERT INTO bookmarks (url, title) VALUES (?, ?)"
-      );
-      const result = insertStmt.run(bookmark.url, bookmark.title);
+    if (existingBookmark) {
       return new Response(
         JSON.stringify({
+          error: "指定されたURLのブックマークは既に登録されています。",
+          message: "指定されたURLのブックマークは既に登録されています。",
           url: bookmark.url,
           title: bookmark.title,
-          id: result.lastInsertRowid,
         }),
         {
-          status: 200,
+          status: 409,
           headers: { "Content-Type": "application/json" },
         }
       );
-    } catch (dbError: unknown) {
-      throw dbError;
     }
+    const insertStmt = db.prepare(
+      "INSERT INTO bookmarks (url, title) VALUES (?, ?)"
+    );
+    const result = insertStmt.run(bookmark.url, bookmark.title);
+    return new Response(
+      JSON.stringify({
+        url: bookmark.url,
+        title: bookmark.title,
+        id: result.lastInsertRowid,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   } catch (error: unknown) {
     return new Response((error as Error).message, {
       status: 500,
