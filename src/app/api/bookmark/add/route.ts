e6fa26e8f9b9177483e2c27f2bd15248c959352d
kubotama <16697573+kubotama@users.noreply.github.com>
2025-06-03 14:01:27 +0900

    fix: 開発環境用のデフォルト値に関するコメントを追加

--- src/app/api/bookmark/add/route.ts
+++ src/app/api/bookmark/add/route.ts
@@ -4,6 +4,7 @@ import { getDb } from "../database";
 
 const ALLOWED_CORS_ORIGIN =
   process.env.ALLOWED_CORS_ORIGIN ||
+  // 開発環境用のデフォルト値。本番環境では必ず環境変数 ALLOWED_CORS_ORIGIN で指定する。
   "chrome-extension://jonckoigjppkhajocdbgfbgjdgffhebf";
 
 export const OPTIONS = async () => {
