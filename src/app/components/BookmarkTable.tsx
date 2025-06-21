3cab268a5d2082282c6af1c690948e685c8f6075
kubotama <16697573+kubotama@users.noreply.github.com>
2025-06-21 15:39:48 +0900

    refactor: BookmarkTableコンポーネントをMUIからカスタム実装に変更し、スタイルを簡素化

--- src/app/components/BookmarkTable.tsx
+++ src/app/components/BookmarkTable.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-
 import { Bookmark, SelectedBookmark } from "../types/Bookmark";
 
 export const BookmarkTable: React.FC<{
@@ -17,21 +7,19 @@ export const BookmarkTable: React.FC<{
   onSelectBookmark: (bookmark: SelectedBookmark) => void;
 }> = ({ bookmarks, onSelectBookmark }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="bookmarks table">
-        <TableHead>
-          <TableRow>
-            <TableCell>タイトル</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {bookmarks.map((bookmark, index) => (
-            <TableRow key={index} onClick={() => onSelectBookmark(bookmark)}>
-              <TableCell size="small">{bookmark.title}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div role="table" className="flex flex-col " aria-label="bookmarks table">
+      <div className="p-1 text-base border">タイトル</div>
+      <div className="flex flex-col">
+        {bookmarks.map((bookmark, index) => (
+          <div
+            className="p-1 text-sm border"
+            key={index}
+            onClick={() => onSelectBookmark(bookmark)}
+          >
+            {bookmark.title}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
