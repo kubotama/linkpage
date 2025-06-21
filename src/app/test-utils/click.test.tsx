8a6bec8f08d42b31e42f854745783d312cf97f54
kubotama <16697573+kubotama@users.noreply.github.com>
2025-06-21 16:09:58 +0900

    refactor: clickBookmark関数から未使用のインポートとコメントを削除

--- src/app/test-utils/click.test.tsx
+++ src/app/test-utils/click.test.tsx
@@ -1,7 +1,6 @@
 import { act } from "react";
 import { Bookmark } from "../types/Bookmark";
-import { fireEvent, screen, waitFor } from "@testing-library/react";
-import { expect } from "vitest";
+import { fireEvent, screen } from "@testing-library/react";
 
 export const clickBookmark = async (bookmark: Bookmark) => {
   // クリックするブックマークを選択（例：2番目のブックマーク）
@@ -13,13 +12,6 @@ export const clickBookmark = async (bookmark: Bookmark) => {
     await act(async () => {
       fireEvent.click(cellWithTitle);
     });
-
-    // await waitFor(() => {
-    //   const urlInput = screen.getByRole("textbox", { name: "url" });
-    //   const titleInput = screen.getByRole("textbox", { name: "title" });
-    //   expect(urlInput).toHaveValue(bookmark.url);
-    //   expect(titleInput).toHaveValue(bookmark.title);
-    // });
   } catch (error) {
     // エラーメッセージに元のエラーを含めるとデバッグが容易になります
     console.error(error); // 元のエラーをログに出力
