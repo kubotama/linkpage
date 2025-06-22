2a160c69e6864d430b49bf4728c236cddfc11533
kubotama <16697573+kubotama@users.noreply.github.com>
2025-06-22 23:11:28 +0900

    feat: ActionButtonおよびBookmarkInputFieldコンポーネントのプロパティにデフォルト値を追加し、クラス名を動的に設定

--- src/app/components/ActionButton.tsx
+++ src/app/components/ActionButton.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-
 type ActionButtonProps = {
   onClick: () => void;
   children: React.ReactNode;
+  widthClass?: string; // 例: "w-28", "w-auto"
+  heightClass?: string; // 例: "h-8", "h-10"
 };
 
 export const ActionButton: React.FC<ActionButtonProps> = ({
   onClick,
   children,
+  widthClass = "w-28", // デフォルト値
+  heightClass = "h-8", // デフォルト値
 }) => {
   return (
     <button
       type="button"
-      className="w-28 bg-blue-500 text-white p-1 rounded-md text-sm h-8"
+      className={`${widthClass} ${heightClass} bg-blue-500 text-white p-1 rounded-md text-sm`}
       onClick={onClick}
     >
       {children}
