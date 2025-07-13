469bc82c2de0c1f236af9bb26f9fc2b5b7a520c1
kubotama <16697573+kubotama@users.noreply.github.com>
2025-07-13 20:10:44 +0900

    refact: ヘルパー関数をシンプルになるように修正した

--- src/app/test-utils/bookmarkTestUtils.tsx
+++ src/app/test-utils/bookmarkTestUtils.tsx
@@ -6,41 +6,14 @@ import { fireEvent, screen, waitFor } from "@testing-library/react";
 import { TITLE_ROLE_NAME, URL_ROLE_NAME } from "./constants";
 import { Bookmark } from "../types/Bookmark";
 
-export const clickBookmark = async (bookmark: Bookmark) => {
-  // クリックするブックマークを選択（例：2番目のブックマーク）
-  // const bookmark = mockBookmarks[1]; // Google
-  try {
-    const cellWithTitle = screen.getByText(bookmark.title);
-
-    // テーブル行のクリックをシミュレート
-    await act(async () => {
-      fireEvent.click(cellWithTitle);
-    });
-
-    // 入力フィールドが設定されるのを待つ
-    await waitFor(() => {
-      expect(screen.getByRole("textbox", { name: URL_ROLE_NAME })).toHaveValue(
-        bookmark.url
-      );
-      expect(
-        screen.getByRole("textbox", { name: TITLE_ROLE_NAME })
-      ).toHaveValue(bookmark.title);
-    });
-  } catch (error) {
-    // エラーメッセージに元のエラーを含めるとデバッグが容易になります
-    console.error(error); // 元のエラーをログに出力
-    throw new Error(
-      `ブックマーク "${bookmark.title}" のテーブル行のクリック処理中にエラーが発生しました。`
-    );
-  }
-};
-
 export const assertBookmarkIsSelected = async (bookmark: Bookmark) => {
   await waitFor(() => {
-    const urlInput = screen.getByRole("textbox", { name: URL_ROLE_NAME });
-    const titleInput = screen.getByRole("textbox", { name: TITLE_ROLE_NAME });
-    expect(urlInput).toHaveValue(bookmark.url);
-    expect(titleInput).toHaveValue(bookmark.title);
+    expect(screen.getByRole("textbox", { name: URL_ROLE_NAME })).toHaveValue(
+      bookmark.url
+    );
+    expect(screen.getByRole("textbox", { name: TITLE_ROLE_NAME })).toHaveValue(
+      bookmark.title
+    );
   });
 };
 
@@ -53,4 +26,24 @@ export const assertNoBookmarkIsSelected = async () => {
       screen.queryByRole("textbox", { name: TITLE_ROLE_NAME })
     ).not.toBeInTheDocument();
   });
+};
+
+export const clickBookmark = async (bookmark: Bookmark) => {
+  // クリックするブックマークを選択（例：2番目のブックマーク）
+  // const bookmark = mockBookmarks[1]; // Google
+  try {
+    const cellWithTitle = screen.getByText(bookmark.title);
+
+    // テーブル行のクリックをシミュレート
+    await act(async () => {
+      fireEvent.click(cellWithTitle);
+    });
+    await assertBookmarkIsSelected(bookmark);
+  } catch (error) {
+    // エラーメッセージに元のエラーを含めるとデバッグが容易になります
+    console.error(error); // 元のエラーをログに出力
+    throw new Error(
+      `ブックマーク "${bookmark.title}" のテーブル行のクリック処理中にエラーが発生しました。`
+    );
+  }
 };
