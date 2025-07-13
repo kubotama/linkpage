b2ff384a68cd77c23cd26ee3a8e956fca77ef61e
kubotama <16697573+kubotama@users.noreply.github.com>
2025-07-13 14:58:40 +0900

    refact: setMessageの初期値の設定を修正した
    デフォルト引数を活用することで、よりシンプルに記述できます。
    TypeScriptの型推論を活用できるため、setMessage の明示的な型定義も省略可能です。

--- src/app/hooks/useErrorMessage.ts
+++ src/app/hooks/useErrorMessage.ts
@@ -4,16 +4,10 @@ export const useErrorMessage = () => {
   const [textMessage, setTextMessage] = useState("");
   const [isError, setIsError] = useState(false);
 
-  const setMessage: (message?: string | undefined, isError?: boolean) => void =
-    useCallback((message = undefined, isError = false) => {
-      if (message === undefined) {
-        setTextMessage("");
-        setIsError(isError);
-        return;
-      }
-      setTextMessage(message);
-      setIsError(isError);
-    }, []);
+  const setMessage = useCallback((message = "", isError = false) => {
+    setTextMessage(message);
+    setIsError(isError);
+  }, []);
 
   const handleErrorClose = useCallback(() => {
     setMessage();
