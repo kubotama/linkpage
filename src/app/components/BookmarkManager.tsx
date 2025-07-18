238a00886c5d97024cf195a65c77671895d6af3c
kubotama <16697573+kubotama@users.noreply.github.com>
2025-07-18 21:44:12 +0900

    fix: legendのキャプションをテストで取得できるように変更した

--- src/app/components/BookmarkManager.tsx
+++ src/app/components/BookmarkManager.tsx
@@ -81,8 +81,8 @@ export const BookmarkManager = () => {
                     onChange={(e) => setTextTitle(e.target.value)}
                   />
                 </div>
-                <legend className="sr-only">{FIELDSET_KEYWORD_LABEL}</legend>
                 <fieldset className="mt-5 flex items-end justify-start border-none p-0">
+                  <legend className="sr-only">{FIELDSET_KEYWORD_LABEL}</legend>
                   <BookmarkInputField
                     id="keyword"
                     placeholder="キーワードを入力してください"
