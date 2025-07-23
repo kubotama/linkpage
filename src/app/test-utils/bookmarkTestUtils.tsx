37785dd5947230edcb671d10922cd0fbaae95944
kubotama <16697573+kubotama@users.noreply.github.com>
2025-07-23 14:47:03 +0900

    fix: エラーメッセージを修正した

--- src/app/test-utils/bookmarkTestUtils.tsx
+++ src/app/test-utils/bookmarkTestUtils.tsx
@@ -139,7 +139,7 @@ export const buildMockBookmarksWithKeywords = (): Bookmark[] => {
 export const getBookmarkByKeywords = (bookmarks: Bookmark[], numKeywords: number = 1): Bookmark => {
   const bookmarkToSelect = bookmarks.find((b) => b.keywords.length > numKeywords);
   if (!bookmarkToSelect) {
-    throw new Error("Test data is missing 'Google' bookmark.");
+    throw new Error(`No bookmark found with more than ${numKeywords} keywords.`);
   }
   return bookmarkToSelect;
 };
