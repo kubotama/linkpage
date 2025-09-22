4c5d0e32653a426569fba6e8b37d57248bedde91
kubotama <16697573+kubotama@users.noreply.github.com>
2025-09-23 00:13:06 +0900

    feat(keyword): キーワード追加ボタンの表示を動的に切り替え
    キーワード入力欄の状態に応じて、キーワード追加ボタンの表示/非表示を切り替えるように変更しました。
    
    - キーワードが未入力の場合、ボタンは表示されません。
    - 入力されたキーワードが既に存在するキーワードの場合、ボタンは表示されません。
    - 上記以外の場合（新規のキーワードが入力された場合）にのみ、ボタンが表示されます。
    
    この変更により、不要なAPIリクエストを防ぎ、ユーザー体験を向上させます。
    また、この変更に伴い、関連するテストコードを修正・追加しました。

--- src/app/components/BookmarkManager.tsx
+++ src/app/components/BookmarkManager.tsx
@@ -89,6 +89,10 @@ export const BookmarkManager = ({ className = "" }: BookmarkManagerProps) => {
     return keywords.filter((k) => !linkedKeywordIds.has(k.keyword_id));
   }, [keywords, linkedKeywordIds]);
 
+  const isEnableAddKeywordButton = useMemo(() => {
+    return textKeyword.length > 0 && !keywords.some((k) => k.keyword_name === textKeyword);
+  }, [textKeyword, keywords]);
+
   return (
     <div className={`mt-5 mb-5 ${className}`}>
       <div className="flex space-x-4">
@@ -142,11 +146,13 @@ export const BookmarkManager = ({ className = "" }: BookmarkManagerProps) => {
                   className="min-w-keyword-input"
                   onChange={(e) => setTextKeyword(e.target.value)}
                 />
-                <div className="ml-2">
-                  <ActionButton onClick={addKeywordClick} className="w-auto">
-                    {ADD_BUTTON_ROLE_NAME}
-                  </ActionButton>
-                </div>
+                {isEnableAddKeywordButton && (
+                  <div className="ml-2">
+                    <ActionButton onClick={addKeywordClick} className="w-auto">
+                      {ADD_BUTTON_ROLE_NAME}
+                    </ActionButton>
+                  </div>
+                )}
               </fieldset>
             </form>
           )}
